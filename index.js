8219b473581623aad3f72e17bd95d85391d21583
SANDY LONG <xuanxuan@MacBook-Pro.local>
2020-12-21 20:42:58 -0500

    toggle for all three questions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 $("#q1").click(function(){
   $("#a1").slideToggle("linear");
-  $("#arrow1-down").toggleClass("collapse"); 
-  $("#arrow1-up").toggleClass("collapse"); 
+  $("#arrow1-down, #arrow1-up").toggleClass("collapse"); 
+})
+
+$("#q2").click(function(){
+  $("#a2").slideToggle("linear");
+  $("#arrow2-down, #arrow2-up").toggleClass("collapse"); 
+})
+
+$("#q3").click(function(){
+  $("#a3").slideToggle("linear");
+  $("#arrow3-down, #arrow3-up").toggleClass("collapse"); 
 })
 
