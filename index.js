7912abe4e1f657e8b69d468d583a038fa58e7a83
SANDY LONG <xuanxuan@MacBook-Pro.local>
2020-12-21 20:57:32 -0500

    using THIS and tree traversal to compress everything in the same function

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-$("#q1").click(function(){
-  $("#a1").slideToggle("linear");
-  $("#arrow1-down, #arrow1-up").toggleClass("collapse"); 
-})
+// $("#q1").click(function(){
+//   $("#a1").slideToggle("linear");
+//   $("#arrow1-down, #arrow1-up").toggleClass("collapse"); 
+// })
 
-$("#q2").click(function(){
-  $("#a2").slideToggle("linear");
-  $("#arrow2-down, #arrow2-up").toggleClass("collapse"); 
-})
+// $("#q2").click(function(){
+//   $("#a2").slideToggle("linear");
+//   $("#arrow2-down, #arrow2-up").toggleClass("collapse"); 
+// })
 
-$("#q3").click(function(){
-  $("#a3").slideToggle("linear");
-  $("#arrow3-down, #arrow3-up").toggleClass("collapse"); 
-})
+// $("#q3").click(function(){
+//   $("#a3").slideToggle("linear");
+//   $("#arrow3-down, #arrow3-up").toggleClass("collapse"); 
+// })
 
+// the above can be compressed into just one function using "this"
+
+
+$(".question").click(function(){
+  $(this).next().fadeToggle("fast");
+  $(this).children().toggleClass("collapse");
+})
